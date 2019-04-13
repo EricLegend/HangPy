967f2b5f5e7ae34957846a10cbeaff079dae81ee
EricLegend <>
2019-04-12 23:51:04 -0500

    Added global vars and turns count

--- hangpy.py
+++ hangpy.py
@@ -1,13 +1,14 @@
 def defineWord():
+    global word
     word = input("Please type a word to play: ")
     print("Your word is: " + word)
     confirmWord();
     return word
 
 def confirmWord():
-    confirm = input("\nPlease confirm (y or n)")
+    confirm = input("Please confirm (y or n): ")
     if confirm == 'y':
-        return gameStart()
+        return setTurnsCount()
     if confirm == 'n':
         return defineWord()
     if confirm != 'y' and confirm != 'n':
@@ -15,5 +16,14 @@ def confirmWord():
         return confirmWord()
     print (confirm)
     return confirm
+
+def setTurnsCount():
+    global tries
+    tries = input("How many tries would you like to give? ")
+    print("Amount of tries: " + tries)
+    return startGame()
+
+def startGame():
+    input("Ready? (y or n): ")
 
 defineWord();
